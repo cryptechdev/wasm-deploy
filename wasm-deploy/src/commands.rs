00c19b1c3696e55850ecdef53f19a72b25df7ef2
Eric Woolsey <ewoolsey@ualberta.ca>
2023-03-29 13:13:17 -0700

    fixed small error

--- wasm-deploy/src/commands.rs
+++ wasm-deploy/src/commands.rs
@@ -261,11 +261,6 @@ where
                 .target_dir
                 .join(generated_file.file_name().unwrap());
             let target_path = shell_completion_dir.join(generated_file.file_name().unwrap());
-            Command::new("rm")
-                .arg(target_path.clone())
-                .spawn()?
-                .wait()
-                .ok();
 
             if Command::new("cp")
                 .arg(source_path)
