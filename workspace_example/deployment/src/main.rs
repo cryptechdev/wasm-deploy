481baa44253009d67897424d392d248a07cb4d96
Eric Woolsey <ewoolsey@ualberta.ca>
2023-03-29 12:55:50 -0700

    updated workspace example

--- workspace_example/deployment/src/main.rs
+++ workspace_example/deployment/src/main.rs
@@ -2,10 +2,14 @@
 pub mod contract;
 pub mod defaults;
 pub mod subcommand;
+use std::path::PathBuf;
+
 use clap::{CommandFactory, FromArgMatches};
 use contract::Contracts;
 use subcommand::{execute_custom_args, CustomSubcommand};
-use wasm_deploy::{cli::Cli, commands::execute_args, error::DeployError};
+use wasm_deploy::{
+    cli::Cli, commands::execute_args, error::DeployError, settings::WorkspaceSettings,
+};
 
 #[tokio::main]
 async fn main() {
@@ -16,11 +20,17 @@ async fn main() {
 }
 
 async fn run() -> Result<(), DeployError> {
+    let package_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let workspace_root = package_root.parent().unwrap();
+    // These settings define where things are located
+    // You can use the builder pattern to change them
+    // Creating a new instance as below will use the defaults which this project represents
+    let settings = WorkspaceSettings::new(workspace_root);
     let cli = Cli::<Contracts, CustomSubcommand>::command();
     let matches = cli.get_matches();
     let args = Cli::<Contracts, CustomSubcommand>::from_arg_matches(&matches)?;
     // You can modify the CLI here
-    execute_args(&args).await?;
+    execute_args(&settings, &args).await?;
     execute_custom_args(&args)?;
     Ok(())
 }
